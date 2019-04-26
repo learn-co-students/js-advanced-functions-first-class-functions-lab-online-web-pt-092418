8260229cf56335f4b83a60712cd29713bafb13e5
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-26 10:55:22 -0400

    Interim checkin

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+  
+};
+const NewFunc = returnFirstTwoDrivers;
+
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2)
+  
+};
